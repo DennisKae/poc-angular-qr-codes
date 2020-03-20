e693b288ccc42401c3f479c96250b822398e41e9
DennisKae <DennisKae@users.noreply.github.com>
2020-03-20 16:06:07 +0100

    bootstrap installiert, Layout angepasst, Seiten angelegt

--- src/poc-angular-qr-codes/src/app/app-routing.module.ts
+++ src/poc-angular-qr-codes/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppModule } from './app.module';
+import { ScanPageComponent } from './pages/scan-page/scan-page.component';
+import { GeneratePageComponent } from './pages/generate-page/generate-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'generate',
+    component: GeneratePageComponent
+  },
+  {
+    path: 'scan',
+    component: ScanPageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'generate',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/poc-angular-qr-codes/src/app/app.module.ts
+++ src/poc-angular-qr-codes/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScanPageComponent } from './pages/scan-page/scan-page.component';
+import { GeneratePageComponent } from './pages/generate-page/generate-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScanPageComponent,
+    GeneratePageComponent
   ],
   imports: [
     BrowserModule,
