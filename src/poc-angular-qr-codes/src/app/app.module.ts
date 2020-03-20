b194aa2ae5815bfa8f8eab4faa6029930d064f11
DennisKae <DennisKae@users.noreply.github.com>
2020-03-20 16:25:29 +0100

    generator implemented

--- src/poc-angular-qr-codes/src/app/app.module.ts
+++ src/poc-angular-qr-codes/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScanPageComponent } from './pages/scan-page/scan-page.component';
 import { GeneratePageComponent } from './pages/generate-page/generate-page.component';
+
+import { NgxKjuaModule } from 'ngx-kjua';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { GeneratePageComponent } from './pages/generate-page/generate-page.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxKjuaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
